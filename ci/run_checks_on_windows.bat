3b4a0138175251a65e87c0a04d7eda8f7ed4ca9e
Sebastian Skałacki <skalee@gmail.com>
2019-12-02 19:22:45 +0100

    wip anotehr

--- ci/run_checks_on_windows.bat
+++ ci/run_checks_on_windows.bat
@@ -1,3 +1,15 @@
 rem call "C:\Program Files (x86)\Microsoft Visual Studio\2017\BuildTools\VC\Auxiliary\Build\vcvarsall.bat" %1
 
-release\fotorelacjonusz.exe -v
+echo -------------------------
+echo -------------------------
+echo Trying AGAIN
+echo -------------------------
+echo -------------------------
+
+pushd release
+echo -------------------------
+dir
+echo -------------------------
+fotorelacjonusz.exe -v
+echo -------------------------
+popd
