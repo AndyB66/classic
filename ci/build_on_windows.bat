dda2ecfc14379891873847529ec783f587497a9a
Sebastian Skałacki <skalee@gmail.com>
2019-12-02 11:50:22 +0100

    try with qt's zlib

--- ci/build_on_windows.bat
+++ ci/build_on_windows.bat
@@ -1,14 +1,14 @@
 call "C:\Program Files (x86)\Microsoft Visual Studio\2017\BuildTools\VC\Auxiliary\Build\vcvarsall.bat" %1
 
-echo -------------
-echo -------------
-echo Building Zlib
-echo -------------
-echo -------------
-pushd third-party\zlib-1.2.11 || goto :fail
-if "%VCVARS%"=="x64" ( nmake -f win32/Makefile.msc AS=ml64 LOC="-DASMV -DASMINF -I." OBJA="inffasx64.obj gvmat64.obj inffas8664.obj" )
-if "%VCVARS%"=="x86" ( nmake -f win32/Makefile.msc LOC="-DASMV -DASMINF" OBJA="inffas32.obj match686.obj" )
-popd
+rem echo -------------
+rem echo -------------
+rem echo Building Zlib
+rem echo -------------
+rem echo -------------
+rem pushd third-party\zlib-1.2.11 || goto :fail
+rem if "%VCVARS%"=="x64" ( nmake -f win32/Makefile.msc AS=ml64 LOC="-DASMV -DASMINF -I." OBJA="inffasx64.obj gvmat64.obj inffas8664.obj" )
+rem if "%VCVARS%"=="x86" ( nmake -f win32/Makefile.msc LOC="-DASMV -DASMINF" OBJA="inffas32.obj match686.obj" )
+rem popd
 
 echo --------------
 echo --------------
@@ -26,7 +26,7 @@ echo Building QuaZIP
 echo ---------------
 echo ---------------
 pushd third-party\quazip\quazip || goto :fail
-qmake CONFIG+=staticlib CONFIG+=c++11 CONFIG+=release INCLUDEPATH+=$$PWD\..\..\zlib-1.2.11 || goto :fail
+qmake CONFIG+=staticlib CONFIG+=c++11 CONFIG+=release INCLUDEPATH+="$$[QT_INSTALL_HEADERS]\QtZlib" || goto :fail
 nmake || goto :fail
 popd
 
@@ -36,7 +36,7 @@ echo Building Fotorelacjonusz
 echo ------------------------
 echo ------------------------
 lrelease fotorelacjonusz.pro || goto :fail
-qmake DEFINES+=QUAZIP_STATIC CONFIG+=release CONFIG+=c++11 INCLUDEPATH+=$$PWD\third-party\zlib-1.2.11 LIBS+="$$PWD\third-party\zlib-1.2.11\zlib.lib" LIBS+="$$PWD\third-party\qtftp\lib\Qt5Ftp.lib" LIBS+="$$PWD\third-party\quazip\quazip\release\quazip.lib" || goto :fail
+qmake DEFINES+=QUAZIP_STATIC CONFIG+=release CONFIG+=c++11 INCLUDEPATH+="$$[QT_INSTALL_HEADERS]\QtZlib" LIBS+="$$PWD\third-party\qtftp\lib\Qt5Ftp.lib" LIBS+="$$PWD\third-party\quazip\quazip\release\quazip.lib" || goto :fail
 nmake || goto :fail
 
 echo -------------------------
