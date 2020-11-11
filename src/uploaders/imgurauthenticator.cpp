2686f1eafc059dd36b61b89f36c6dc194e543a84
Sebastian Skałacki <skalee@gmail.com>
2020-11-11 23:02:32 +0100

    n class

---
+++ src/uploaders/imgurauthenticator.cpp
@@ -0,0 +1,6 @@
+#include "imgurauthenticator.h"
+
+ImgurAuthenticator::ImgurAuthenticator()
+{
+
+}
---
+++ src/uploaders/imgurauthenticator.h
@@ -0,0 +1,12 @@
+#ifndef IMGURAUTHENTICATOR_H
+#define IMGURAUTHENTICATOR_H
+
+
+class ImgurAuthenticator
+{
+	Q_OBJECT
+public:
+	ImgurAuthenticator();
+};
+
+#endif // IMGURAUTHENTICATOR_H
